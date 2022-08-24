ae408069fff9e45cb2094c2df7acb5fbedfaa192
unknown <alissonjosef@gmail.com>
2022-08-24 19:10:01 -0300

    link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,12 +5,12 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <h1>blog.Apiki</h1>
+        <h1>blog.<span style={{color:'#61bafb'}}>Apiki</span></h1>
         <nav>
           <Link href="/">
             <a >Home</a>
           </Link>
-          <Link href="/posts" prefetch>
+          <Link href="/posts">
             <a >Post</a>
           </Link>
         </nav>
